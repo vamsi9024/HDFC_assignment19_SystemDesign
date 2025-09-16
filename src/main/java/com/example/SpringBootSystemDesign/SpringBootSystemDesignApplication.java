51690cad3f3a215b951b2328db4da0b5e1a4459d
vamsi9024 <petavamsikrishna9024@gmail.com>
2025-09-16 22:36:50 +0530

    commited

---
+++ src/main/java/com/example/SpringBootSystemDesign/SpringBootSystemDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSystemDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSystemDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSystemDesignApplication.class, args);
+	}
+
+}
